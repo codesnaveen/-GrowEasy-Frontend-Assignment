90cff3f57065fc65c99fed0a51c2f3f846ee9659
Naveen Menon <116652452+codesnaveen@users.noreply.github.com>
2024-08-04 16:46:10 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const isProd = process.env.NODE_ENV === 'production';
 const repoName = 'groweasy-frontend-assignment'; // Your repository name
 
 module.exports = {
+  output: 'export',
+  
   assetPrefix: isProd ? `/${repoName}/` : '',
   basePath: isProd ? `/${repoName}` : '',
   trailingSlash: true,
